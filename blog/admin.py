49b92f6a8cc3b1c6a600f5005a11ba44cef5fc2d
Omarkhaledm21 <Omarkhaledm21@gmail.com>
2022-08-30 18:39:14 +0200

    .

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import Post, Author, Tag
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter= ("author","tags","date")
+    list_display = ("title", "author","date")
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email")
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,42 @@
+from tkinter import CASCADE
 from django.db import models
-
+from datetime import date
+from django.urls import reverse
+from django.core.validators import MinLengthValidator
 # Create your models here.
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return f"{self.caption}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    def __str__(self) -> str:
+        return f"Author: {self.first_name} {self.last_name} Email: {self.email}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    slug = models.SlugField(unique=True, null=False)
+    excerpt = models.TextField(blank=True, default="")
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}, date = {self.date}, Slug: {self.slug}"
+
+    def get_absolute_url(self):
+        return reverse("post-detail-page", args=[self.slug])
